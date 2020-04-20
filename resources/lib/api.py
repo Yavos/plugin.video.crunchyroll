a5b77274a0486089e40afffa36af2f2fcd3da040
Yavos <yavos@arcor.de>
2020-04-20 22:04:11 +0200

    fix missing import

--- resources/lib/api.py
+++ resources/lib/api.py
@@ -16,6 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import json
+import sys
 from os import remove
 from os.path import join
 
--- resources/lib/controller.py
+++ resources/lib/controller.py
@@ -16,6 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import re
+import sys
 import ssl
 import time
 import inputstreamhelper
